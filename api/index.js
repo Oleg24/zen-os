60dfb0b38aab61af731d978b9e679b2f942cb0e4
Oleg <o.yanchinskiy@gmail.com>
2018-01-24 20:55:49 -0800

    removed node modules from git repo

--- api/index.js
+++ api/index.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 /* app configurations */
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 mongoose.connect(process.env.MONGO_DB);
 
-require('./routes')(app);
+require("./routes")(app);
 
-app.get('*', (req, res)=>{
-	res.send('welcome to contract.io!');
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
 app.listen(process.env.CONTRACTS_PORT, () => {
